84a772d37020ba8fd8f306a2a97d39c4d09b0f1e
JKwas99 <79404613+JKwas99@users.noreply.github.com>
2024-04-15 17:15:37 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,194 +1,173 @@
 let button = document.getElementsByClassName("hamburger")
 let sidebar = document.getElementsByClassName("menu-list")
 let logo_pic = document.getElementById("menu-logo")
-let background = document.getElementsByClassName("main-box")
-let up = document.getElementsByClassName("up")
-let iconMove = document.getElementsByClassName("cursorFun")
+let element = document.querySelector('.moreAboutUs');
+let span = document.querySelector('.robotyczne');
+let scrollDown = document.querySelector(".icon-angle-double-down")
+let ab = document.querySelector(".ab")
+let bef = document.getElementsByClassName("bef")
+const anim = document.getElementsByClassName("anim")
+let desc = document.getElementsByClassName("desc")
 
-addEventListener("mousemove", (e) => {
-    mousePos = { x: e.clientX, y: e.clientY };
-    iconMove[0].style.left = `${mousePos.x+20}px`
-    iconMove[0].style.top = `${mousePos.y+15}px`
-});
-
-
-up[0].addEventListener('click', ()=>{
-    button[0].classList.remove("hamburger-active")
-    sidebar[0].classList.remove("list-active")
-    logo_pic.classList.remove("logo-active")
-})
 button[0].addEventListener('click', ()=>{
     button[0].classList.toggle("hamburger-active")
     sidebar[0].classList.toggle("list-active")
     logo_pic.classList.toggle("logo-active")
 })
 
-document.addEventListener("scroll", (event) => {
-    let box = document.getElementsByClassName("first")
-    if(window.scrollY > box[0].clientHeight){
-        up[0].classList.add("up-active");
-        up[0].addEventListener('click',()=>{
-            document.body.scrollTop = 0;
-            document.documentElement.scrollTop = 0;
-        })
-    }
-    if(window.scrollY < box[0].clientHeight){
-        up[0].classList.remove("up-active");
-    }
+for(let i=0;i<400;i++){
+  const kropka = document.createElement('span');
+  kropka.classList.add('kropka');
+  if (i % 2 === 0) {
+    bef[0].appendChild(kropka);
+  }
+  else{
+    bef[1].appendChild(kropka);
+  }
+  
+}
+
+const kropki = document.querySelectorAll('.kropka');
+
+kropki.forEach(kropka => {
+  const pozycjaTop = Math.random() * bef[0].clientHeight;
+  const pozycjaLeft = Math.random() * bef[0].clientWidth;
+  const przesunieciePoziome = Math.random() * 20 - 10;
+  const przesunieciePionowe = Math.random() * 20 - 10; 
+  kropka.style.transition = "transform 2s linear";
+  kropka.style.transform = `translate(${przesunieciePoziome}px, ${przesunieciePionowe}px)`;
+  kropka.style.top = pozycjaTop + 'px';
+  kropka.style.left = pozycjaLeft + 'px';
+  kropka.addEventListener('transitionend', () => {
+  kropka.style.transform = `translate(0px, 0px)`;
+  setTimeout(() => {
+        const przesunieciePoziome = Math.random() * 20 - 10;
+        const przesunieciePionowe = Math.random() * 20 - 10;
+        kropka.style.transform = `translate(${przesunieciePoziome}px, ${przesunieciePionowe}px)`;
+    }, 100);
+});
 });
 
-let left = 0, timer = 0, timer2 = 0, currentPosition = 0;
+scrollDown.addEventListener('click', ()=>{
+  document.getElementById('ab').scrollIntoView({ behavior: 'smooth', block: 'start' });
+});
+if(window.innerWidth>700){
+  document.addEventListener("scroll",()=>{
+    i=window.scrollY*0.75;
+    let under = document.querySelector(".under")
+    ab.style.transform = `translateX(${i}px)`;
+    let x = i*2
+    under.style.transform = `translateX(-${x}px)`;
+    if(i/10<80)
+      ab.style.opacity = `${i*5}%`
+    ab.style.setProperty('--procent', `${i}%`);
+    const scrollTop = window.scrollY;
+  setTimeout(()=>{
+    anim[0].style.transform = `translateY(${scrollTop * 0.5}px)`;
+  },100)
+  setTimeout(()=>{
+    anim[1].style.transform = `translateY(${scrollTop * 0.5}px)`;
+  },300)
+  })
+}else{
+  document.addEventListener("scroll",()=>{
+    i=window.scrollY*0.08;
+    let under = document.querySelector(".under")
+    ab.style.transform = `translateX(${i}px)`;
+    let x = i*2
+    under.style.transform = `translateX(-${x}px)`;
+    if(i/10<80)
+      ab.style.opacity = `${i*5}%`
+    ab.style.setProperty('--procent', `${i}%`);
+    const scrollTop = window.scrollY;
+  setTimeout(()=>{
+    if(scrollTop * 0.35<bef[0].clientHeight-anim[0].clientHeight){
+      anim[0].style.transform = `translateY(${scrollTop * 0.35}px)`;}
+  },100)
+  setTimeout(()=>{
+    if(scrollTop * 0.35<bef[0].clientHeight-anim[1].clientHeight){
+      anim[1].style.transform = `translateY(${scrollTop * 0.35}px)`;}
+  },300)
+  })
+}
 
-let startTime = null;
-const duration = 7000; // 7 sekund w milisekundach
+bef[0].addEventListener("click", ()=>{
+  let desc = document.getElementsByClassName("desc")
+  let inf = document.getElementsByClassName("inf")
+  let przed = document.getElementsByClassName("przed")
 
-const photos = ["grafika/transport1.png", "grafika/transport2.png", "grafika/transport3.png", "grafika/transport4.png", "grafika/transport5.png"]
-let counter = 0
+  desc[0].classList.toggle("descVis")
+  inf[0].classList.toggle("infVis")
+  przed[0].classList.toggle("pzedVis")
+})
+
+let text = "oznaj nas lepiej.";
+let text2 = `PROGRAMISTYCZNO-ROBOTYCZNE`
+let animationInProgress = false;
+let animationTimeout;
+let animationInProgress2 = false;
+let animationTimeout2;
 
 
-function moveBackground(timestamp) {
-    if (!startTime) {
-        startTime = timestamp;
+function typeWriter(text, i) {
+    if (i < text.length && animationInProgress) {
+        element.innerHTML += text.charAt(i);
+        animationTimeout = setTimeout(function() {
+            typeWriter(text, i + 1);
+        }, 40);
+      }
+}
+
+function typeWriter2(text2, i) {
+  if (i < text2.length && animationInProgress2) {
+    if(i==15){
+      span.innerHTML+=`<span id="block">-</span>`
     }
-
-    const progress = timestamp - startTime;
-
-    if (progress <= duration) {
-        const percentage = (progress / duration) * 100;
-        background[0].style.backgroundPosition = percentage + '% 0';
-        requestAnimationFrame(moveBackground);
-    } else {
-        startTime = null; // Zaczynamy od nowa
-        if(counter<photos.length-1){
-            background[0].style.backgroundImage = `url('${photos[counter+1]}')`; 
-            background[0].style.transition = "opacity 0.5s linear;"
-            counter++
-        }
-        else{
-            counter = 0
-            background[0].style.backgroundImage = `url('${photos[counter]}')`; 
-            background[0].style.transition = "opacity 0.5s linear;"
-        }
-        requestAnimationFrame(moveBackground);
+    else{
+      span.innerHTML += text2.charAt(i);
+    }
+    if(i==15 && window.innerWidth<600){
+      span.innerHTML+=`<br>`
+      //element.style.transform = "translateY(-50px)"
+    }
+      animationTimeout2 = setTimeout(function() {
+          typeWriter2(text2, i + 1);  
+      }, 60);
+    }
+    else{
+      animationTimeout2 = setTimeout(function(){
+        span.innerHTML = ""
+        typeWriter2(text2,0)
+      }, 1500)
     }
 }
 
-let list_item = document.getElementsByClassName("list-item")
-let box = document.getElementsByClassName("first")
-let sec1 = document.getElementsByClassName("second")
-let offer = document.getElementsByClassName("offer")
-let form = document.getElementsByClassName("form")
-let callN = document.getElementsByClassName("callN")
+setInterval(()=>{
+  if (!animationInProgress2) {
+    span.textContent = ""
+    animationInProgress2 = true;
+    typeWriter2(text2, 0);
+  }
+},4)
 
-list_item[0].addEventListener('click',()=>{
-    button[0].classList.remove("hamburger-active")
-    sidebar[0].classList.remove("list-active")
-    logo_pic.classList.remove("logo-active")
-})
-list_item[1].addEventListener('click',()=>{
-    button[0].classList.remove("hamburger-active")
-    sidebar[0].classList.remove("list-active")
-    logo_pic.classList.remove("logo-active")
-})
-list_item[2].addEventListener('click',()=>{
-    button[0].classList.remove("hamburger-active")
-    sidebar[0].classList.remove("list-active")
-    logo_pic.classList.remove("logo-active")
-})
-list_item[3].addEventListener('click',()=>{
-    button[0].classList.remove("hamburger-active")
-    sidebar[0].classList.remove("list-active")
-    logo_pic.classList.remove("logo-active")
-})
 
-document.addEventListener("scroll", (event) => {
-    if(window.scrollY<box[0].clientHeight){
-        for(let i=0;i<list_item.length;i++){
-            list_item[i].classList.remove("active-menu")
+    element.addEventListener('mouseover', function() {
+      element.textContent = "P"
+      if (!animationInProgress) {
+        animationInProgress = true;
+        typeWriter(text, 0);
+      }
+    });
+    element.addEventListener('click', function() {
+        element.textContent = "Po"
+        if (!animationInProgress) {
+          animationInProgress = true;
+          typeWriter(text, 0);
         }
-        list_item[0].classList.add("active-menu")
-        callN[0].classList.remove("callN-noactive")
-    }
-    if(window.scrollY>box[0].clientHeight/3){
-        let boxy = document.getElementsByClassName("boxyJS")
-        let boxi = document.getElementsByClassName("boxI")
-        let boxh = document.getElementsByClassName("boxH")
-        let boxp = document.getElementsByClassName("boxP")
-        boxi[0].classList.add("boxes-show1")
-        boxi[1].classList.add("boxes-show2")
-        boxi[2].classList.add("boxes-show3")
-        boxh[0].classList.add("boxesH-show1")
-        boxh[1].classList.add("boxesH-show2")
-        boxh[2].classList.add("boxesH-show3")
-        boxp[0].classList.add("boxesP-show1")
-        boxp[1].classList.add("boxesP-show2")
-        boxp[2].classList.add("boxesP-show3")
-        callN[0].classList.remove("callN-noactive")
-    }
-    if(window.scrollY>box[0].clientHeight/2){
-        let h3offer = document.getElementsByClassName("offerh3")
-        h3offer[0].classList.add("h3offer-show")
-        let imgBox = document.getElementsByClassName("boxyJS")
-        imgBox[0].classList.add("boxes-show1I")
-        imgBox[1].classList.add("boxes-show2I")
-        imgBox[2].classList.add("boxes-show3I")
-        imgBox[3].classList.add("boxes-show4I")
-        callN[0].classList.remove("callN-noactive")
-    }
-    if(window.scrollY > box[0].clientHeight+sec1[0].clientHeight){
-        for(let i=0;i<list_item.length;i++){
-            list_item[i].classList.remove("active-menu")
-        }
-        list_item[1].classList.add("active-menu")
-        callN[0].classList.remove("callN-noactive")
-    }
-    if(window.scrollY > box[0].clientHeight+sec1[0].clientHeight+offer[0].clientHeight/2){
-        let wyc = document.getElementsByClassName("wycH")
-        wyc[0].classList.add("boxes-show1")
-        callN[0].classList.remove("callN-noactive")
-    }
-    if(window.scrollY >box[0].clientHeight+sec1[0].clientHeight+offer[0].clientHeight){
-        for(let i=0;i<list_item.length;i++){
-            list_item[i].classList.remove("active-menu")
-        }
-        list_item[2].classList.add("active-menu")
-        callN[0].classList.remove("callN-noactive")
-    }
-    if(window.scrollY >box[0].clientHeight+sec1[0].clientHeight+offer[0].clientHeight+form[0].clientHeight/2){
-        for(let i=0;i<list_item.length;i++){
-            list_item[i].classList.remove("active-menu")
-        }
-        list_item[3].classList.add("active-menu")
-        callN[0].classList.add("callN-noactive")
-    }
+      });
 
-});
-
-
-requestAnimationFrame(moveBackground);
-
-let trasa = document.getElementById("trasa")
-let telefon = document.getElementById("kontakt")
-let opis = document.getElementById("opis")
-let send = document.getElementsByClassName("form_button")
-
-send[0].addEventListener('click', ()=>{
-    if(trasa.value=="" || telefon.value=="" || opis.value==""){
-        console.log("Wypisz poprawne dane!")
-        alert("Sprawdź, czy podane pola są uzupełnione!");
-    }
-    else{
-        console.log("Formularz zostal wysłany!")
-        alert("Formularz zostal wysłany poprawnie!");
-        var formData = new FormData();
-        formData.append("trasa", trasa);
-        formData.append("telefon", telefon);
-        formData.append("opis", opis);
-
-        fetch("/submit_form", {
-            method: "POST",
-            body: formData
-        });
-
-    }
-})
+    element.addEventListener('mouseout', function() {
+      element.textContent = "Poznaj nas lepiej."
+      animationInProgress = false;
+      clearTimeout(animationTimeout);
+    });
