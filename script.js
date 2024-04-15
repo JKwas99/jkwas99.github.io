7bfeb697748826ca01f00d3499e17ca16b9c7735
JKwas99 <79404613+JKwas99@users.noreply.github.com>
2024-04-15 19:00:17 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -86,79 +86,59 @@ window.addEventListener('scroll', () => {
   // Pobierz aktualną pozycję przewijania strony w dół
   const scrollTop = window.scrollY || window.pageYOffset;
 
-  // Oblicz procent przewinięcia strony w dół
-  const scrollHeight = document.documentElement.scrollHeight - window.innerHeight;
-  const scrollPercent = (scrollTop / scrollHeight) * 100;
 
+  // Oblicz procent przewinięcia strony w dół
+  const scrollHeight = window.innerHeight;
+  const scrollPercent = (scrollTop / scrollHeight) * 100;
+  console.log(scrollPercent)
   // Oblicz pozycję docelową na osi X
   const translateX = calculateTranslateX(scrollPercent);
-  const translateX2 = calculateTranslateX(scrollPercent)*(-1);
   ab.style.opacity = `${translateX}%`
 
   // Ustaw styl transformacji dla elementu DIV
-  movingDiv.style.transform = `translateX(${translateX}px)`;
-  let x = translateX*2
-  under.style.transform = `translateX(-${x}px)`;
+  if(window.innerWidth>700){
+    ab.style.transform = `translateX(${translateX}px)`;
+    let x = translateX*2
+    under.style.transform = `translateX(-${x}px)`;
+  }
+  else{
+    ab.style.transform = `translateX(${translateX/5}px)`;
+    let x = translateX/4
+    under.style.transform = `translateX(-${x}px)`;
+  }
+
+
+  
 
 });
 
-/*document.addEventListener("scroll",()=>{
-  i=window.scrollY/window.clientWidth;
-  let under = document.querySelector(".under")
-  ab.style.transform = `translateX(${i}%)`;
-  let x = i*2
-  under.style.transform = `translateX(-${x}px)`;
-  if(i/10<80)
-    ab.style.opacity = `${i*5}%`
-  ab.style.setProperty('--procent', `${i}%`);
-  const scrollTop = window.scrollY;
-setTimeout(()=>{
-  anim[0].style.transform = `translateY(${scrollTop * 0.5}px)`;
-},100)
-setTimeout(()=>{
-  anim[1].style.transform = `translateY(${scrollTop * 0.5}px)`;
-},300)
-})
 
-/*if(window.innerWidth>700){
+if(window.innerWidth>700){
   document.addEventListener("scroll",()=>{
-    i=window.scrollY*0.75;
-    let under = document.querySelector(".under")
-    ab.style.transform = `translateX(${i}px)`;
-    let x = i*2
-    under.style.transform = `translateX(-${x}px)`;
-    if(i/10<80)
-      ab.style.opacity = `${i*5}%`
-    ab.style.setProperty('--procent', `${i}%`);
     const scrollTop = window.scrollY;
-  setTimeout(()=>{
-    anim[0].style.transform = `translateY(${scrollTop * 0.5}px)`;
-  },100)
-  setTimeout(()=>{
-    anim[1].style.transform = `translateY(${scrollTop * 0.5}px)`;
-  },300)
+    setTimeout(()=>{
+      if(scrollTop<mainBox[0].clientHeight+anim[0].clientHeight+70)
+        anim[0].style.transform = `translateY(${scrollTop * 0.5}px)`;
+    },100)
+    setTimeout(()=>{
+      if(scrollTop<mainBox[0].clientHeight+anim[1].clientHeight+70)
+        anim[1].style.transform = `translateY(${scrollTop * 0.5}px)`;
+    },300)
   })
 }else{
   document.addEventListener("scroll",()=>{
-    i=window.scrollY*0.08;
-    let under = document.querySelector(".under")
-    ab.style.transform = `translateX(${i}px)`;
-    let x = i*2
-    under.style.transform = `translateX(-${x}px)`;
-    if(i/10<80)
-      ab.style.opacity = `${i*5}%`
-    ab.style.setProperty('--procent', `${i}%`);
     const scrollTop = window.scrollY;
+    console.log(mainBox[0].clientHeight)
   setTimeout(()=>{
-    if(scrollTop * 0.35<bef[0].clientHeight-anim[0].clientHeight){
-      anim[0].style.transform = `translateY(${scrollTop * 0.35}px)`;}
+    if(scrollTop<mainBox[0].clientHeight+anim[0].clientHeight+50)
+      anim[0].style.transform = `translateY(${scrollTop * 0.5}px)`;
   },100)
   setTimeout(()=>{
-    if(scrollTop * 0.35<bef[0].clientHeight-anim[1].clientHeight){
-      anim[1].style.transform = `translateY(${scrollTop * 0.35}px)`;}
+    if(scrollTop<mainBox[0].clientHeight+anim[1].clientHeight+50)
+      anim[1].style.transform = `translateY(${scrollTop * 0.5}px)`;
   },300)
   })
-}*/
+}
 
 desc[0].addEventListener("click", ()=>{
   let desc = document.getElementsByClassName("desc")
@@ -178,7 +158,7 @@ desc[1].addEventListener("click", ()=>{
 
   desc[1].classList.toggle("descVis")
   inf[1].classList.toggle("infVis")
-  przed[1].classList.toggle("pzedVis")
+  przed[1].classList.toggle("przedVis")
 })
 
 
